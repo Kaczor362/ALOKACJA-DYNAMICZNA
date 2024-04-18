005b56113544af16fc900abcb3cb54c5a6c904c6
Kaczor <kaczorowski0000@gmail.com>
2024-04-18 20:18:07 +0200

    arrayh

---
+++ array.h
@@ -0,0 +1,18 @@
+//
+// Created by student on 22.03.2024.
+//
+
+#ifndef UNTITLED8DX_ARRAY_H
+#define UNTITLED8DX_ARRAY_H
+
+void insert(char array[]);
+
+void show(char array[]);
+
+int lenght(char *array);
+
+char small(char *array);
+
+void clear(void);
+
+#endif //UNTITLED8DX_ARRAY_H
